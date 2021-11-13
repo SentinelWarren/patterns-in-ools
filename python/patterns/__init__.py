06da0061c0b2fecd75e236c6a967cadbbaf6ab24
SentinelWarren <warrenkalolo@gmail.com>
2021-11-13 04:28:57 +0300

    Update singleton.py & Add singleton.ipynb

--- python/patterns/__init__.py
+++ python/patterns/__init__.py
@@ -1,1 +1,9 @@
 __version__ = '0.1.0'
+__all__ = [
+            'Singleton',
+            'Logger',
+            'Prototype',
+        ]
+
+from patterns.singleton.singleton import Singleton, Logger
+from patterns.prototype.prototype import Prototype
