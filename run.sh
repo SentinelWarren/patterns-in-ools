f80f10dedb27c13af13292fa1b4408a464107ae2
SentinelWarren <warrenkalolo@gmail.com>
2021-11-14 18:09:11 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,43 +2,43 @@
 start=`date +%s`
 
 printf "\nRunning the Python version : \n"
-pytest pytthon/
+pytest pytthon
 
-printf "\nRunning the Ruby version : \n"
+# printf "\nRunning the Ruby version : \n"
 
-printf "\nRunning the C version : \n"
+# printf "\nRunning the C version : \n"
 
-printf "\nRunning the Haskell version : \n"
+# printf "\nRunning the Haskell version : \n"
 
-printf "\nRunning the Rust version : \n"
+# printf "\nRunning the Rust version : \n"
 
-printf "\nRunning the Java version : \n"
+# printf "\nRunning the Java version : \n"
 
-printf "\nRunning the Javascript version : \n"
+# printf "\nRunning the Javascript version : \n"
 
-printf "\nRunning the PHP version : \n"
+# printf "\nRunning the PHP version : \n"
 
-printf "\nRunning the Scala version : \n"
+# printf "\nRunning the Scala version : \n"
 
-printf "\nRunning the C++ version : \n"
+# printf "\nRunning the C++ version : \n"
 
-printf "\nRunning the Go version : \n"
+# printf "\nRunning the Go version : \n"
 
-printf "\nRunning the C# version : \n"
+# printf "\nRunning the C# version : \n"
 
-printf "\nRunning the Kotlin version : \n"
+# printf "\nRunning the Kotlin version : \n"
 
-printf "\nRunning the Scheme version : \n"
+# printf "\nRunning the Scheme version : \n"
 
-printf "\nRunning the Clojure version : \n"
+# printf "\nRunning the Clojure version : \n"
 
-printf "\nRunning the Elixir version : \n"
+# printf "\nRunning the Elixir version : \n"
 
-printf "\nRunning the Dart Version : \n"
+# printf "\nRunning the Dart Version : \n"
 
-printf "\nRunning the TypeScript version : \n"
+# printf "\nRunning the TypeScript version : \n"
 
-printf "\nRunning the Swift version : \n"
+# printf "\nRunning the Swift version : \n"
 
 end=`date +%s`
 runtime=$((end-start))
