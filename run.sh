a11a984714d4d93dff7df02375ac2ed31ee3050b
SentinelWarren <warrenkalolo@gmail.com>
2021-11-22 16:41:09 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 start=`date +%s`
 
-printf "\nRunning Python  version tests : \n"
-pytest python
+printf "\nRunning Python version tests : \n"
+python3 -m pytest python/
 
-# printf "\nRunning Ruby  version tests : \n"
+# printf "\nRunning Ruby version tests : \n"
 
-# printf "\nRunning Java  version tests : \n"
+# printf "\nRunning Java version tests : \n"
 
-# printf "\nRunning JavaScript  version tests : \n"
+# printf "\nRunning JavaScript version tests : \n"
 
-# printf "\nRunning PHP  version tests : \n"
+# printf "\nRunning PHP version tests : \n"
 
-# printf "\nRunning Scala  version tests : \n"
+# printf "\nRunning Scala version tests : \n"
 
-# printf "\nRunning C++  version tests : \n"
+# printf "\nRunning C++ version tests : \n"
 
-# printf "\nRunning Go  version tests : \n"
+# printf "\nRunning Go version tests : \n"
 
-# printf "\nRunning C#  version tests : \n"
+# printf "\nRunning C# version tests : \n"
 
-# printf "\nRunning Kotlin  version tests : \n"
+# printf "\nRunning Kotlin version tests : \n"
 
-# printf "\nRunning Dart  version tests : \n"
+# printf "\nRunning Dart version tests : \n"
 
-# printf "\nRunning TypeScript  version tests : \n"
+# printf "\nRunning TypeScript version tests : \n"
 
-# printf "\nRunning Swift  version tests : \n"
+# printf "\nRunning Swift version tests : \n"
 
 end=`date +%s`
 runtime=$((end-start))
